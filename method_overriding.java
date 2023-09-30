8b65fd9e9fb81f6ef9ec5d3c41f1baa8d4241d4d
Gopal Nivrutti Dhage <105532298+gopalthedev@users.noreply.github.com>
2023-09-30 22:52:49 +0530

    Create method_overriding.java

---
+++ method_overriding.java
@@ -0,0 +1,38 @@
+// Java Program to implement
+// Method Overriding
+import java.io.*;
+
+// Base Class
+class Animal {
+	void eat()
+	{
+		System.out.println("eat() method of base class");
+		System.out.println("eating.");
+	}
+}
+
+// Inherited Class
+class Dog extends Animal {
+	void eat()
+	{
+		System.out.println("eat() method of derived class");
+		System.out.println("Dog is eating.");
+	}
+}
+
+class MethodOverridingEx {
+	// Main Function
+	public static void main(String args[])
+	{
+		Dog d1 = new Dog();
+		Animal a1 = new Animal();
+
+		d1.eat();
+		a1.eat();
+
+		Animal animal = new Dog();
+		// eat() method of animal class is overridden by
+		// base class eat()
+		animal.eat();
+	}
+}
