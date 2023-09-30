e711f00f3c98423f9b1f2e88fbff69466404cadf
Gopal Nivrutti Dhage <105532298+gopalthedev@users.noreply.github.com>
2023-10-01 00:22:43 +0530

    Update method_overloading.java

--- method_overloading.java
+++ method_overloading.java
@@ -3,17 +3,17 @@
 
 public class Sum {
 	// Overloaded sum(). This sum takes two int parameters
-	public int sum(int x, int y) { return (x + y); }
+	public int add(int x, int y) { return (x + y); }
 
 	// Overloaded sum(). This sum takes three int parameters
-	public int sum(int x, int y, int z)
+	public int add(int x, int y, int z)
 	{
 		return (x + y + z);
 	}
 
 	// Overloaded sum(). This sum takes two double
 	// parameters
-	public double sum(double x, double y)
+	public double add(double x, double y)
 	{
 		return (x + y);
 	}
